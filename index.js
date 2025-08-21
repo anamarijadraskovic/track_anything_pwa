4551f31aff3ff0f2bb7df84d641cddafe1be6874
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-08-21 13:33:01 +0000

    Push a lead to a Firebase Realtime Database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,7 +8,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
+const database = getDatabase(app);
+// Reference is a specific location in the database where we can push data
+const referenceInDB = ref(database, "item")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -34,7 +36,7 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
+    push(referenceInDB, inputEl.value)
     console.log(inputEl.value)
     inputEl.value = ""
-
 })
