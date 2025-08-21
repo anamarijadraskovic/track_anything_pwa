7e016ca13f4afd27539333d75b2b01dbff2f98da
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-08-21 13:43:12 +0000

    Get values from Firebase database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,6 +16,10 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+
+onValue(referenceInDB, function (snapshot) {
+    console.log(snapshot.val())
+})
 
 function render(leads) {
     let listItems = ""
